5c5f033018b9b2be98e1da7469dab3fef261eb9b
changcheng <15713997121@126.com>
2018-12-01 19:44:49 +0800

    修改后提交

---
+++ src/com/itcast/domain/Product.java
@@ -0,0 +1,13 @@
+package com.itcast.domain;
+
+public class Product {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
