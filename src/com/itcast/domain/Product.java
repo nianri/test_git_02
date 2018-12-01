0ca32b145030d441b3e77bcc3041d65765290aa7
changcheng <15713997121@126.com>
2018-12-01 20:27:19 +0800

    修改后提交

--- src/com/itcast/domain/Product.java
+++ src/com/itcast/domain/Product.java
@@ -3,6 +3,16 @@ package com.itcast.domain;
 public class Product {
     private String name;
 
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getName() {
         return name;
     }
