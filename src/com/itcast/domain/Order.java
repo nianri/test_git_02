cf72a17059fcfcfd749a3c83de826755a9228822
changcheng <15713997121@126.com>
2018-12-02 09:25:45 +0800

    第二次提交到本地仓库

--- src/com/itcast/domain/Order.java
+++ src/com/itcast/domain/Order.java
@@ -5,6 +5,7 @@ public class Order {
     public static void main(String[] args) {
         System.out.println("-----------------------------");
         System.out.println("++++++++++++++++++++++++++++++++++");
+        System.out.println("*****************************");
     }
 
 }
